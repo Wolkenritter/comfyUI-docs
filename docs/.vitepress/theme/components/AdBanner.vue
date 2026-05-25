528dd3456bc230d4d620e9a3b41c793a79cfdcc0
MoonFerry <korewangggg@foxmail.com>
2026-05-25 21:41:46 +0800

    add google adsense

--- docs/.vitepress/theme/components/AdBanner.vue
+++ docs/.vitepress/theme/components/AdBanner.vue
@@ -2,8 +2,12 @@
   <div class="ad-container" :class="[position, type]">
     <!-- Google AdSense -->
     <template v-if="type === 'adsense'">
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2644340141064615"
-     crossorigin="anonymous"></script>
+      <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-2644340141064615"
+        data-ad-slot="4695046211"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
     </template>
 
     <!-- 百度联盟 -->
